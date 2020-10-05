af92e4d6972970e6e7f52ae273e0a17f29d9d996
Fan Weng <fanweng02@gmail.com>
2020-10-04 21:01:59 -0400

    Keep only one copy of xv6 source for all homeworks from Homework 4.

---
+++ homeworks/hw04-lazy-page-alloc/README.md
@@ -0,0 +1,3 @@
+# Homework 4 - Lazy Page Allocation
+
+From this homework and the later ones, only one copy of xv6 source code *homeworks/xv6-public* will be kept. All changes will be added to that copy. This baseline is copied from *hw03-syscall*.
---
+++ homeworks/xv6-public/date.c
@@ -0,0 +1,20 @@
+/* user program "date" */
+
+#include "types.h"
+#include "user.h"
+#include "date.h"
+
+int
+main(int argc, char *argv[])
+{
+  struct rtcdate r;
+
+  if (date(&r)) {
+    printf(2, "date failed\n");
+    exit();
+  }
+
+  printf(1, "%d-%d-%d %d:%d:%d\n", r.year, r.month, r.day, r.hour, r.minute, r.second);
+
+  exit();
+}
